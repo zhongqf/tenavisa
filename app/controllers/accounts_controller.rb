cef1f8475585121eec28aded859ca96372ecc379
universac <universac@yahoo.com>
2010-02-28 22:19:05 +0800

    Need login to access*

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,6 @@
 class AccountsController < ApplicationController
   # Be sure to include AuthenticationSystem in Application Controller instead
-  include AuthenticatedSystem
+  skip_before_filter :login_required
   
 
   # render new.rhtml
