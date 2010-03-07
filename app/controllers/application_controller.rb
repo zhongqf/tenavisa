6ef1362b1fe6bbe46060d960a2a99ea1e9d2bed0
universac <universac@yahoo.com>
2010-03-07 17:51:52 +0800

    move *_required and current_* into controller; minor modified layout header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,31 @@ class ApplicationController < ActionController::Base
   before_filter :login_required
   
   layout 'layout'
+  
+  def educatee_required
+    ( !! current_educatee ) || access_denied
+  end
+  
+  def current_educatee
+    return current_account.loginable if current_account && current_account.loginable.is_a?(Educatee)
+    nil
+  end
+  
+  def educator_required
+    ( !!current_educator ) || access_denied
+  end
+  
+  def current_educator
+    return current_account.loginable if current_account && current_account.loginable.is_a?(Educator)
+    nil
+  end
+  
+  def operator_required
+    ( !!current_operator ) || access_denied
+  end
+  
+  def current_operator
+    return current_account.loginable if current_account && current_account.loginable.is_a?(Operator)
+    nil
+  end
 end
