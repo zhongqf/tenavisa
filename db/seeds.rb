2760cf82dbd98ac39796a1a0bddb057bff3d9553
universac <universac@yahoo.com>
2010-05-08 16:36:37 +0800

    can view elements on typus now

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,20 +50,28 @@ puts "   Generate sample operators ..."
   account.activate!
   
 puts "   Generate sample pages ..."
-  pages = Page.create!([{:title => "Basic Info"}, {:title => "Education"}, {:title => "Passport"}])
+  pages = Page.create!([{:title => "Basic Info"}, {:title => "Education"}, {:title => "Passport"}, {:title => "Sample"}])
 
 puts "   Generate sample elements ..."
-e11 = Element.create!(:key => "nationality", :kind => "string", :title_cn => "国籍", :title_jp => "国籍", :page => pages.first)
-e12 = Element.create!(:key => "birthday", :kind => "date", :title_cn => "出生年月", :title_jp => "生年月日", :page => pages.first)
-e13 = Element.create!(:key => "name_in_chinese", :kind => "string", :title_cn => "中文姓名", :title_jp => "中国語名前", :page => pages.first)
-e14 = Element.create!(:key => "name_in_english", :kind => "string", :title_cn => "英文姓名", :title_jp => "英語名前", :page => pages.first)
-e15 = Element.create!(:key => "sex", :kind => "selector", :params => "{:values => ['男','女']}", :title_cn => "性别", :title_jp => "性別", :page => pages.first)
-e16 = Element.create!(:key => "place_of_birth", :kind => "string", :title_cn => "出生地", :title_jp => "出生地", :page => pages.first)
-e17 = Element.create!(:key => "photo", :kind => "file", :title_cn => "照片", :title_jp => "写真", :page => pages.first)
-e21 = Element.create!(:key => "address_in_japan", :kind => "string", :title_cn => "在日居住地", :title_jp => "連絡先", :page => pages.second)
-e22 = Element.create!(:key => "tel", :kind => "string", :title_cn => "电话号码", :title_jp => "電話番号", :page => pages.second)
-e23 = Element.create!(:key => "cellular", :kind => "string", :title_cn => "携带电话号码", :title_jp => "携帯番号", :page => pages.second)
-e24 = Element.create!(:key => "passport_no", :kind => "string", :title_cn => "护照号码", :title_jp => "旅券番号", :page => pages.last)
-e25 = Element.create!(:key => "passport_expiration", :kind => "string", :title_cn => "护照有效期", :title_jp => "旅券有効期限", :page => pages.last)
+e11 = Element.create!(:key => "nationality", :kind => "string", :title_cn => "国籍", :title_jp => "国籍", :page => pages.first, :position => 1, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e12 = Element.create!(:key => "birthday", :kind => "date", :title_cn => "出生年月", :title_jp => "生年月日", :page => pages.first, :position => 2, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e13 = Element.create!(:key => "name_in_chinese", :kind => "string", :title_cn => "中文姓名", :title_jp => "中国語名前", :page => pages.first, :position => 3, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e14 = Element.create!(:key => "name_in_english", :kind => "string", :title_cn => "英文姓名", :title_jp => "英語名前", :page => pages.first, :position => 4, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e15 = Element.create!(:key => "sex", :kind => "selector", :params => "{:values => ['男','女']}", :title_cn => "性别", :title_jp => "性別", :page => pages.first, :position => 5, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e16 = Element.create!(:key => "place_of_birth", :kind => "string", :title_cn => "出生地", :title_jp => "出生地", :page => pages.first, :position => 6, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e17 = Element.create!(:key => "photo", :kind => "file", :title_cn => "照片", :title_jp => "写真", :page => pages.first, :position => 7, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e21 = Element.create!(:key => "address_in_japan", :kind => "string", :title_cn => "在日居住地", :title_jp => "連絡先", :page => pages.second, :position => 1, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e22 = Element.create!(:key => "tel", :kind => "string", :title_cn => "电话号码", :title_jp => "電話番号", :page => pages.second, :position => 2, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e23 = Element.create!(:key => "cellular", :kind => "string", :title_cn => "携带电话号码", :title_jp => "携帯番号", :page => pages.second, :position => 3, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e31 = Element.create!(:key => "passport_no", :kind => "string", :title_cn => "护照号码", :title_jp => "旅券番号", :page => pages[2], :position => 1, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e32 = Element.create!(:key => "passport_expiration", :kind => "date", :title_cn => "护照有效期", :title_jp => "旅券有効期限", :page => pages[2], :position => 2, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+
+e41 = Element.create!(:key => "boolean_sample", :kind => "boolean", :title_cn => "Boolean", :title_jp => "Boolean", :page => pages[3], :position => 1, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "date_sample", :kind => "date", :title_cn => "Date", :title_jp => "Date", :page => pages[3], :position => 2, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "file_sample", :kind => "file", :title_cn => "File", :title_jp => "File", :page => pages[3], :position => 3, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "radio_sample", :kind => "radio", :title_cn => "Radio", :title_jp => "Radio", :page => pages[3], :params => "{:values => ['Option 1', 'Option 2', 'Option 3']}" , :position => 4, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "selector_sample", :kind => "selector", :title_cn => "Selector", :title_jp => "Selector", :page => pages[3], :params => "{:values => ['Value A', 'Value B', 'Value C']}" , :position => 5, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "string_sample", :kind => "string", :title_cn => "String", :title_jp => "String", :page => pages[3], :position => 6, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
+e41 = Element.create!(:key => "text_sample", :kind => "text", :title_cn => "Text", :title_jp => "Text", :page => pages[3], :position => 7, :description => "", :enabled => true, :validation_script => "", :prawn_output_script => "", :created_at => Time.now, :updated_at => Time.now)
 
 puts "Over."
